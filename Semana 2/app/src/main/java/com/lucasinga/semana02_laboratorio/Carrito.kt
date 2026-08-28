687046d09ebb0157d9fcfe6c21785f5cdcdceb9b
Lucas Inga <lucas.inga@tecsup.edu.pe>
2026-08-27 23:12:59 -0500

    Agrega data class Producto y variables del carrito
    Se define la data class Producto con nombre, precio y cantidad.
    Se crean 4 productos y se agregan a la lista del carrito.

--- Semana 2/app/src/main/java/com/lucasinga/semana02_laboratorio/Carrito.kt
+++ Semana 2/app/src/main/java/com/lucasinga/semana02_laboratorio/Carrito.kt
@@ -1,5 +1,26 @@
 package com.lucasinga.semana02_laboratorio
 
+data class Producto(
+    val nombre: String,
+    val precio: Double,
+    var cantidad: Int
+)
+
 fun main() {
     println("   Carrito de compras - Tienda Tecsup")
+
+    val nombreCliente = "Lucas Inga"
+    val carrito = mutableListOf<Producto>()
+    println("Cliente: $nombreCliente")
+    println()
+
+    carrito.add(Producto("Audifonos Samsung", 180.0, 1))
+    carrito.add(Producto("Monitor", 800.0, 1))
+    carrito.add(Producto("Celular", 1200.0, 1))
+    carrito.add(Producto("Disco Duro", 250.0, 2))
+
+    for (producto in carrito) {
+        println("Producto agregado: ${producto.nombre}")
+    }
+    println()
 }
